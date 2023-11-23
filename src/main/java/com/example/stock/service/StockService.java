cc6faa29ff9a9539e9d4a8a7419930b889d509fe
sujeong <msj1506@naver.com>
2023-11-23 09:57:05 +0900

    재고 감소

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -1,7 +1,9 @@
 package com.example.stock.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 
 @Service
@@ -13,9 +15,13 @@ public class StockService {
 		this.stockRepository = stockRepository;
 	}
 
+	@Transactional
 	public void decrease(Long id, Long quantity) {
 		//stock 조회
 		//재고를 감소한 뒤
 		//갱신된 값을 저장
+		Stock stock = stockRepository.findById(id).orElseThrow();
+		stock.decrease(quantity);
+		stockRepository.saveAndFlush(stock);
 	}
 }
