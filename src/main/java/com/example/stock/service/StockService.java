a93c55e8c9bd56b2d7775ddbff9d8d8c9770e3e7
sujeong <msj1506@naver.com>
2023-11-23 10:43:46 +0900

    Synchronized 문제점

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -16,6 +16,8 @@ public class StockService {
 	}
 
 	//synchronized: 해당 메소드에 하나의 쓰레드만 접근 가능
+	//synchronized 문제점: 서버가 2대 이상일 경우 데이터 접근 여러대, 각 프로세스 내에서만 보장
+	//서버1가 재고 100 가져가, 서버2도 재고 100 가져가 문제
 	//트랜잭션 decrease 완료 후 실제 데이터베이스가 업데이트 되기 전 다른 쓰레드가 decrease 호출
 	// @Transactional
 	public synchronized void decrease(Long id, Long quantity) {
