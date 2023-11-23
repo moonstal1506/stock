39d3d0c5c6f7a890c04650ce883367f7afb80977
sujeong <msj1506@naver.com>
2023-11-23 10:27:06 +0900

    Synchronized 이용

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -15,8 +15,10 @@ public class StockService {
 		this.stockRepository = stockRepository;
 	}
 
-	@Transactional
-	public void decrease(Long id, Long quantity) {
+	//synchronized: 해당 메소드에 하나의 쓰레드만 접근 가능
+	//트랜잭션 decrease 완료 후 실제 데이터베이스가 업데이트 되기 전 다른 쓰레드가 decrease 호출
+	// @Transactional
+	public synchronized void decrease(Long id, Long quantity) {
 		//stock 조회
 		//재고를 감소한 뒤
 		//갱신된 값을 저장
