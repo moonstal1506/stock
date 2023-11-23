b6f848c351c77d2cfaed30fe61238ff7774a4c1f
sujeong <msj1506@naver.com>
2023-11-23 10:15:13 +0900

    재고 감소 레이스 컨디션

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -2,6 +2,10 @@ package com.example.stock.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -35,5 +39,34 @@ class StockServiceTest {
 		stockService.decrease(1L, 1L);
 		Stock stock = stockRepository.findById(1L).orElseThrow();
 		assertEquals(99, stock.getQuantity());
+	}
+
+	@Test
+	public void 동시에_100개의_요청() throws InterruptedException {
+		int threadCount = 100;
+		//멀티쓰레드 이용, 비동기로 실행 작업 단순화 하여 사용하게 도와줌
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+		//100개의 요청이 끝날때까지 기다려야 함
+		//다른 쓰레드에서 진행중인 작업이 완료될 때까지 대기할 수 있도록 도와주는 클래스
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					stockService.decrease(1L, 1L);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+		latch.await();
+		Stock stock = stockRepository.findById(1L).orElseThrow();
+
+		//레이스 컨디션: 둘이상의 쓰레드가 공유데이터에 access, 동시에 변경하려고 할 때 발생
+		//쓰레드1: 100에서 1감소 99
+		//쓰레드2: 쓰레드1이 감소 되기전 재고를 가져가-> 100에서 1감소 99
+		//해결방법: 하나의 쓰레드가 작업이 완료된 이후 다른 쓰레드가 데이터에 접근 할 수 있도록
+		assertEquals(0, stock.getQuantity());
 	}
 }
